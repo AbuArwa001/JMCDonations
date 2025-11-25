4c22f63b2feeb8c1340c05298335e8cfd89b668b
AbuArwa001 <khalfanathman12@yahoo.com>
2025-11-25 16:35:41 +0300

    Update user view

--- users/views.py
+++ users/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render
-
+from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import status
+from rest_framework.decorators import api_view, permission_classes
+from .models import Users
+from .serializers import FCMTokenSerializer, UserUUIDSerializer, UserSerializer
 
 class FCMTokenUpdateView(APIView):
     permission_classes = [IsAuthenticated]
@@ -15,6 +17,46 @@ class FCMTokenUpdateView(APIView):
             token = serializer.validated_data['fcm_token']
             request.user.fcm_token = token
             request.user.save()
-            return Response({'status': 'FCM token updated'}, status=status.HTTP_200_OK)
+            
+            # Return user UUID for Firebase Analytics setup
+            user_data = UserUUIDSerializer({
+                'user_uuid': request.user.id,
+                'email': request.user.email,
+                'full_name': request.user.full_name
+            })
+            
+            return Response({
+                'status': 'FCM token updated',
+                'user': user_data.data
+            }, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+    def get(self, request, *args, **kwargs):
+        """Get current FCM token"""
+        return Response({
+            'fcm_token': request.user.fcm_token,
+            'user_uuid': request.user.id
+        })
+
+class UserProfileView(APIView):
+    """Get user profile including UUID for Firebase setup"""
+    permission_classes = [IsAuthenticated]
+    
+    def get(self, request):
+        serializer = UserSerializer(request.user)
+        return Response(serializer.data)
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def update_fcm_token(request):
+    """Alternative function-based view for FCM token update"""
+    serializer = FCMTokenSerializer(data=request.data)
+    if serializer.is_valid():
+        request.user.fcm_token = serializer.validated_data['fcm_token']
+        request.user.save()
+        return Response({
+            'status': 'success',
+            'user_uuid': str(request.user.id),
+            'message': 'FCM token updated successfully'
+        })
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
