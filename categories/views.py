83366e32a18767ab018f4cb58a191cef390bad6e
AbuArwa001 <khalfanathman12@yahoo.com>
2025-11-28 10:10:05 +0300

    feat: implement CategoryViewSet with permission handling and created_by field

--- categories/views.py
+++ categories/views.py
@@ -1,7 +1,19 @@
 from rest_framework import viewsets
+
+from donations.permissions import AllowAny, IsAdminUser
 from .models import Categories
 from .serializers import CategorySerializer
 
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Categories.objects.all()
     serializer_class = CategorySerializer
+
+    def get_permissions(self):
+        if self.action in ['create', 'update', 'partial_update', 'destroy']:
+            permission_classes = [IsAdminUser]
+        else:
+            permission_classes = [AllowAny]
+        return [permission() for permission in permission_classes]
+
+    def perform_create(self, serializer):
+        serializer.save(created_by=self.request.user)
