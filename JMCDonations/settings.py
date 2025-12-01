6daeb174d81d68e08a97e935635f9b396f361d69
AbuArwa001 <khalfanathman12@yahoo.com>
2025-12-01 08:22:36 +0300

    feat: add production URL to CORS_ALLOWED_ORIGINS for enhanced security

--- JMCDonations/settings.py
+++ JMCDonations/settings.py
@@ -44,6 +44,7 @@ ALLOWED_HOSTS = ["jmcdonations.onrender.com", "localhost", "127.0.0.1"]
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000", 
     "http://127.0.0.1:3000",
+    "https://jmcdonations.onrender.com",
 ]
 
 # Application definition
