f9c082e04852d989e3db3057e298a24024915260
AbuArwa001 <khalfanathman12@yahoo.com>
2025-12-06 15:33:54 +0300

    feat: update URL routing for ratings and users, enhance build script with admin claims setup

--- build.sh
+++ build.sh
@@ -19,6 +19,8 @@ python manage.py loaddata additional_transactions.json
 python manage.py loaddata additional_donation_data.json
 python manage.py loaddata ratings_additional_data.json
 
+python set_admin_claims.py
+
 # python manage.py loaddata initial_data.json
 # Bash
 
