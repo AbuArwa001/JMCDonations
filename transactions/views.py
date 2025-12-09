284ab1b5445bc3105e3a1455fb0eee82336e6170
AbuArwa001 <khalfanathman12@yahoo.com>
2025-12-09 16:35:52 +0300

    feat: enhance stk_push method to handle error responses and create transaction records

--- transactions/views.py
+++ transactions/views.py
@@ -4,11 +4,13 @@ from rest_framework.response import Response
 from .models import Transactions
 from .serializers import TransactionSerializer
 from .daraja import MpesaClient
+from JMCDonations.authentication import FirebaseAuthentication
 
 
 class TransactionViewSet(viewsets.ModelViewSet):
     queryset = Transactions.objects.order_by("-donated_at")
     serializer_class = TransactionSerializer
+    authentication_classes = []
 
     @action(detail=False, methods=["post"])
     def stk_push(self, request):
@@ -21,9 +23,22 @@ class TransactionViewSet(viewsets.ModelViewSet):
             return Response(
                 {"error": "Missing required fields"}, status=status.HTTP_400_BAD_REQUEST
             )
+        
 
         cl = MpesaClient()
         response = cl.stk_push(
             phone_number, amount, account_reference, transaction_desc
         )
+        if response.get("error"):
+            return Response(
+                response, status=status.HTTP_400_BAD_REQUEST
+            )
+        elif response.get("ResponseCode") != "0":
+            tansaction = Transactions.objects.create(
+                amount=amount,
+                payment_method="M-Pesa",
+                payment_status="Pending",
+            )
+            serializer = self.get_serializer(tansaction)
+            response.update(serializer.data)
         return Response(response)
