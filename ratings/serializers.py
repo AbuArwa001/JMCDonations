ce7d5a4f3e153a692f96f55d46909f7550aa25fa
AbuArwa001 <khalfanathman12@yahoo.com>
2025-12-15 00:07:34 +0300

    feat: enhance DashboardSummaryView with total collected amounts and donation trends for better analytics

--- ratings/serializers.py
+++ ratings/serializers.py
@@ -3,11 +3,14 @@ from .models import Ratings
 
 
 class RatingSerializer(serializers.ModelSerializer):
+    user_name = serializers.CharField(source="user.full_name", read_only=True)
+
     class Meta:
         model = Ratings
         fields = (
             "id",
             "user",
+            "user_name",
             "donation",
             "comment",
             "rating",
