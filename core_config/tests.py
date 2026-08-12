77f08620d5c1520f8f529ffdaa616403ab5e73f4
AbuArwa001 <khalfanathman12@yahoo.com>
2026-08-12 04:17:31 +0300

    test: implement pytest suite for prayer time calculation and override functionality

--- core_config/tests.py
+++ core_config/tests.py
@@ -1,3 +1,22 @@
-from django.test import TestCase
+import pytest
+from django.urls import reverse
+from rest_framework.test import APIClient
+from core_config.models import AppFeature
 
-# Create your tests here.
+@pytest.mark.django_db
+class TestFeatureToggles:
+    def test_feature_toggle_read(self):
+        AppFeature.objects.create(name='khutba', is_active=True)
+        AppFeature.objects.create(name='events', is_active=False)
+        
+        client = APIClient()
+        url = reverse('features-list')
+        response = client.get(url)
+        
+        assert response.status_code == 200
+        data = response.json()
+        assert len(data) >= 2
+        
+        features = {f['name']: f['is_active'] for f in data}
+        assert features.get('khutba') is True
+        assert features.get('events') is False
--- prayer_times/tests.py
+++ prayer_times/tests.py
@@ -1,3 +1,45 @@
-from django.test import TestCase
+import pytest
+import datetime
+from django.urls import reverse
+from prayer_times.models import City, PrayerCalculationSettings, PrayerTimeOverride
+from rest_framework.test import APIClient
 
-# Create your tests here.
+@pytest.mark.django_db
+class TestPrayerTimes:
+    def setup_method(self):
+        self.client = APIClient()
+        self.city = City.objects.create(
+            name="Nairobi",
+            latitude=-1.2921,
+            longitude=36.8219,
+            timezone="Africa/Nairobi"
+        )
+        PrayerCalculationSettings.objects.create(
+            calculation_method='MUSLIM_WORLD_LEAGUE'
+        )
+
+    def test_prayer_time_calculation(self):
+        url = reverse('prayer-times-calc')
+        response = self.client.get(url, {'city': self.city.id, 'date': '2023-10-10'})
+        
+        assert response.status_code == 200
+        data = response.json()
+        assert 'fajr' in data
+        assert 'dhuhr' in data
+        assert 'maghrib' in data
+
+    def test_prayer_time_override(self):
+        override_time = datetime.time(5, 30)
+        PrayerTimeOverride.objects.create(
+            city=self.city,
+            date=datetime.date(2023, 10, 10),
+            prayer_name='fajr',
+            overridden_time=override_time
+        )
+        
+        url = reverse('prayer-times-calc')
+        response = self.client.get(url, {'city': self.city.id, 'date': '2023-10-10'})
+        
+        assert response.status_code == 200
+        data = response.json()
+        assert data['fajr'] == '05:30:00'
