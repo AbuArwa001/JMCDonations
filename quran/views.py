6c709a68dd6973ee86a75d047f4d607cd5fb14ec
AbuArwa001 <khalfanathman12@yahoo.com>
2026-08-12 04:12:51 +0300

    feat: implement Reciter and SurahAudio viewsets with role-based permissions and filtering

--- quran/views.py
+++ quran/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import AllowAny, IsAdminUser
+from django_filters.rest_framework import DjangoFilterBackend
+from .models import Reciter, SurahAudio
+from .serializers import ReciterSerializer, SurahAudioSerializer
 
-# Create your views here.
+class ReciterViewSet(viewsets.ModelViewSet):
+    queryset = Reciter.objects.all()
+    serializer_class = ReciterSerializer
+    
+    def get_permissions(self):
+        if self.action in ['list', 'retrieve']:
+            permission_classes = [AllowAny]
+        else:
+            permission_classes = [IsAdminUser]
+        return [permission() for permission in permission_classes]
+
+class SurahAudioViewSet(viewsets.ModelViewSet):
+    queryset = SurahAudio.objects.all()
+    serializer_class = SurahAudioSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['surah_number', 'reciter']
+    
+    def get_permissions(self):
+        if self.action in ['list', 'retrieve']:
+            permission_classes = [AllowAny]
+        else:
+            permission_classes = [IsAdminUser]
+        return [permission() for permission in permission_classes]
